b3ec87db669f2fe9c473807ce2433ca9481aac52
mahdi-tendjaoui <mahdi.tendjaoui@city.ac.uk>
2026-04-09 20:44:31 +0100

    Fix typos
    title

--- src/main/java/IPOS_SA_CoreAPI.java
+++ src/main/java/IPOS_SA_CoreAPI.java
@@ -1,16 +1,14 @@
-import ISupplierAPI.*;
-
-public class IPOS_SA_CoreAPI implements submitPurchaseOrder {
+public class IPOS_SA_CoreAPI {
 
 	public IPOS_SA_CoreAPI() {
 		// TODO - implement IPOS_SA_CoreAPI.IPOS_SA_CoreAPI
 		throw new UnsupportedOperationException();
 	}
 
-	public ProductList[] getProductCatalogue() {
+	/*public ProductList[] getProductCatalogue() {
 		// TODO - implement IPOS_SA_CoreAPI.getProductCatalogue
 		throw new UnsupportedOperationException();
-	}
+	}*/
 
 	/**
 	 * 
@@ -25,14 +23,14 @@ public class IPOS_SA_CoreAPI implements submitPurchaseOrder {
 	 * 
 	 * @param orderID
 	 */
-	public string getDeliveryStatus(string orderID) {
+	public String getDeliveryStatus(String orderID) {
 		// TODO - implement IPOS_SA_CoreAPI.getDeliveryStatus
 		throw new UnsupportedOperationException();
 	}
 
-	public Invoice[] getOutstandingInvoices() {
+	/*public Invoice[] getOutstandingInvoices() {
 		// TODO - implement IPOS_SA_CoreAPI.getOutstandingInvoices
 		throw new UnsupportedOperationException();
-	}
+	}*/
 
 }
