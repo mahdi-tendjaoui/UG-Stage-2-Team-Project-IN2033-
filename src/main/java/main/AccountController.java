1d0f447eb26becb16ff1b29c49c5495671716e50
sanjeebilly05 <Sanjeeb.Gurung@city.ac.uk>
2026-04-12 01:47:19 +0100

    updated accounts and account controller

---
+++ src/main/java/main/AccountController.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountController {
+    private List<Accounts> accounts;
+
+    public AccountController() {
+        this.accounts = new ArrayList<>();
+    }
+
+    //Create an account, assuming the user is new
+    public void createAccount(String username, String password, String role) {
+    }
+
+    public void searchAccount(String username) {
+    }
+}
