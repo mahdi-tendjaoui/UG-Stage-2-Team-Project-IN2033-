5e0059bd30e74af454bd1b7fb5ad3d0138db1046
mahdi-tendjaoui <mahdi.tendjaoui@city.ac.uk>
2026-04-12 01:03:14 +0100

    Add Launcher and AccountView
    Added Launcher which will serve as the launch point for IPOS-SA's GUI.
    
    Added a provisional GUI for IPOS-SA's account page with AccountView.

---
+++ src/main/java/com/example/ipossa/views/AccountView.java
@@ -0,0 +1,16 @@
+package com.example.ipossa.views;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AccountView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
