f87322cd4b1132f35b839df8df79d137c4e8787d
mahdi-tendjaoui <mahdi.tendjaoui@city.ac.uk>
2026-04-14 20:54:53 +0100

    Create OrderView class and fxml file
    Created a view for Orders and its respective fxml file.

--- src/main/java/com/prototype/ipossa/views/OrderView.java
+++ src/main/java/com/prototype/ipossa/views/OrderView.java
@@ -1,6 +1,14 @@
 package com.prototype.ipossa.views;
 
+import javafx.fxml.FXML;
+
+import com.prototype.ipossa.systems.Orders.OrderService;
+
 public class OrderView
 {
+    @FXML
+    private void showOrders()
+    {
 
+    }
 }
