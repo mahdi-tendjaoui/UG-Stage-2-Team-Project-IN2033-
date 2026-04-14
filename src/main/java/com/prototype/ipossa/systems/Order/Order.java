872686902e68cbd5adb28b1d2cd17ba2428beaa0
mahdi-tendjaoui <mahdi.tendjaoui@city.ac.uk>
2026-04-14 17:08:05 +0100

    Refactor Order
    title

--- src/main/java/com/prototype/ipossa/systems/Order/Order.java
+++ src/main/java/com/prototype/ipossa/systems/Order/Order.java
@@ -24,6 +24,7 @@ public class Order {
         this.orderDate = orderDate;
         this.items = new ArrayList<>();
     }
+
     public double calculateTotalAmount() {
         double totalAmount = 0;
         for (OrderItem item : items) {
@@ -35,58 +36,75 @@ public class Order {
         items.removeIf(item -> item.getProductID().equals(productID));
         totalAmount = calculateTotalAmount();
     }
+
     public void addItem(OrderItem item) {
         items.add(item);
         totalAmount = calculateTotalAmount();
     }
+
     public List<OrderItem> getItems() {
         return items;
     }
+
     public enum OrderStatus {
         ACCEPTED,
         READY_TO_DISPATCH,
         DISPATCHED,
         DELIVERED
     }
+
     public OrderStatus getStatus() {
         return status;
     }
+
     public void setStatus(OrderStatus status) {
         this.status = status;
     }
+
     public String getOrderID() {
         return orderID;
     }
+
     public void setOrderId(String orderID) {
         this.orderID = orderID;
     }
+
     public String getMerchantID() {
         return merchantID;
     }
+
     public void setMerchantID(String merchantID) {
         this.merchantID = merchantID;
     }
+
     public String getMerchantName() {
         return merchantName;
     }
+
     public void setMerchantName(String merchantName) {
         this.merchantName = merchantName;
     }
+
     public double getTotalAmount() {
         return totalAmount;
     }
+
     public void setTotalAmount(double totalAmount) {
         this.totalAmount = totalAmount;
     }
+
     public String getInvoiceID() {
         return invoiceID;
     }
+
     public void setInvoiceID(String invoiceID) {
         this.invoiceID = invoiceID;
     }
+
     public String getOrderDate() {
         return orderDate;
     }
+
     public void setOrderDate(String orderDate) {
         this.orderDate = orderDate;
     }
