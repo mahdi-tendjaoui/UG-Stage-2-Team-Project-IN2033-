479018b6d26f95d190c095dae3feb66f671ea246
Adil Arun <adil.arun@gmail.com>
2026-04-19 20:59:17 +0100

    Cleaning code

--- IPOS-SA/src/main/java/module-info.java
+++ IPOS-SA/src/main/java/module-info.java
@@ -5,17 +5,15 @@ module com.prototype.ipossa {
     requires java.sql;
 
     exports com.prototype.ipossa;
-    exports com.prototype.ipossa.views;
     exports com.prototype.ipossa.ui;
     exports com.prototype.ipossa.ui.pages;
     exports com.prototype.ipossa.systems;
     exports com.prototype.ipossa.systems.ACC;
     exports com.prototype.ipossa.systems.CAT;
     exports com.prototype.ipossa.systems.ORD;
-    exports com.prototype.ipossa.systems.RPRT;
+//    exports com.prototype.ipossa.systems.RPRT;
 
     opens com.prototype.ipossa to javafx.fxml, javafx.graphics;
-    opens com.prototype.ipossa.views to javafx.fxml;
     opens com.prototype.ipossa.ui to javafx.fxml, javafx.graphics;
     opens com.prototype.ipossa.ui.pages to javafx.fxml, javafx.graphics;
     opens com.prototype.ipossa.systems.ACC to javafx.fxml, javafx.graphics;
